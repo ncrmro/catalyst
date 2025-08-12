f7a7b705d20a785fa394d387b26ebce9ba6fddab
Nicholas Romero <ncrmro@gmail.com>
2025-08-12 18:02:03 -0500

    chroe: disable authjs callbacks for now

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -14,18 +14,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
     })
   ],
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
-    },
-    async session({ session, token }) {
-      // Send properties to the client
-      session.accessToken = token.accessToken as string
-      return session
-    },
-  },
+  // callbacks: {
+  //   async jwt({ token, account }) {
+  //     // Persist the OAuth access_token to the token right after signin
+  //     if (account) {
+  //       token.accessToken = account.access_token
+  //     }
+  //     return token
+  //   },
+  //   async session({ session, token }) {
+  //     // Send properties to the client
+  //     session.accessToken = token.accessToken as string
+  //     return session
+  //   },
+  // },
 })
