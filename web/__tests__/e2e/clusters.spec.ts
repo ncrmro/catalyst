7a9fcc10a8f9dff7d0429cbc430249ba291fbd7e
Nicholas Romero <ncrmro@gmail.com>
2025-08-12 02:09:03 -0500

    feat: add simple e2e test

---
+++ web/__tests__/e2e/clusters.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Clusters Page', () => {
+  test('should display clusters page header', async ({ page }) => {
+    await page.goto('/clusters');
+    await expect(page.locator('h1')).toContainText('CLUSTERS');
+  });
+}); 
