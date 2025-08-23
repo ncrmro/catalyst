beba44265a56708f0c5f853965a1ad1f0ea79411
Nicholas Romero <ncrmro@gmail.com>
2025-08-22 22:50:30 -0500

    chore: simplify e2e tests for now

--- web/__tests__/e2e/clusters.spec.ts
+++ web/__tests__/e2e/clusters.spec.ts
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
+import { loginWithDevPassword } from './helpers';
 
 test.describe('Clusters Page', () => {
+  test.beforeEach(async ({ page }, testInfo) => {
+    await loginWithDevPassword(page, testInfo);
+  });
+
   test('should display clusters page header', async ({ page }) => {
     await page.goto('/clusters');
     await expect(page.locator('h1')).toContainText('Clusters');
