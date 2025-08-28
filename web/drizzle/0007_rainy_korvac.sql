3043eeb93dc205661920c3de10413b9b4e4aa079
Nicholas Romero <ncrmro@gmail.com>
2025-08-28 17:09:05 -0500

    chore: regenerate migrations

--- web/drizzle/0007_rainy_korvac.sql
+++ web/drizzle/0007_rainy_korvac.sql
@@ -3,8 +3,7 @@ CREATE TABLE "project_manifests" (
 	"repo_id" text NOT NULL,
 	"path" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "project_manifests_pkey" PRIMARY KEY("project_id","repo_id","path")
+	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 ALTER TABLE "project_manifests" ADD CONSTRAINT "project_manifests_project_id_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."project"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
